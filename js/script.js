ed91edac05247489bfb2e9387e494ce5353eff09
usmile581 <gourley581@yahoo.com>
2017-02-22 14:12:26 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  $("navbarToggle").blur(function(event) {
+  $("#navbarToggle").blur(function(event) {
      var screenWidth = window.innerWidth;
      if (screenWidth < 768) {
         $("#collapsable-nav").collapse('hide');
